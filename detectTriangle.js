90d3a69bbd68811df82266340e84db6d222f7211
SideeID <sandipratama164026@gmail.com>
2024-04-28 09:24:31 +0700

    Add input validation for detectTriangle function

--- detectTriangle.js
+++ detectTriangle.js
@@ -1,5 +1,6 @@
-function detectTriangle() {
-
+function detectTriangle(a, b, c) {
+  if (typeof a !== 'number' || typeof b !== 'number' || typeof c !== 'number') {
+    throw new Error('Input must be a number');
+  }
 }
-
 export default detectTriangle;
