c868b1414cf1ea8fb3a5b711217259d69e8d8e3b
SideeID <sandipratama164026@gmail.com>
2024-04-28 09:24:45 +0700

    Add input validation for detectTriangle function and import vitest in detectTriangle.test.js

--- detectTriangle.test.js
+++ detectTriangle.test.js
@@ -10,4 +10,19 @@
  *   - should return "Segitiga Sembarang" when all input are different
  */
 
-import 
+import { describe, it, expect } from 'vitest';
+import detectTriangle from './detectTriangle';
+
+describe('detectTriangle function', () => {
+  it('should throw error when input is not a number', () => {
+    expect(() => detectTriangle('a', 2, 3)).toThrowError(
+      'Input must be a number'
+    );
+    expect(() => detectTriangle(1, 'b', 3)).toThrowError(
+      'Input must be a number'
+    );
+    expect(() => detectTriangle(1, 2, 'c')).toThrowError(
+      'Input must be a number'
+    );
+  });
+});
